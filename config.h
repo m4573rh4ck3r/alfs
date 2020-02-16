bc361fb7362370b319def29916135ca6f351effa
H8CKR <h8ckr@protonmail.com>
2020-02-16 16:50:24 +0100

    add config header file

---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "automated-linux-from-scratch"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "m4573rh4ck3r@protonmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Automated-Linux-From-Scratch"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Automated-Linux-From-Scratch 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "automated-linux-from-scratch"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
