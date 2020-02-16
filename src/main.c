9a75c2f6be3853fda1a16e41ebacf1fe35e1b577
H8CKR <h8ckr@protonmail.com>
2020-02-16 23:43:14 +0100

    get lfs passwd entry

--- src/main.c
+++ src/main.c
@@ -2,10 +2,43 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pwd.h>
+#include <errno.h>
+
+#define LFS "/mnt/lfs"
+
+struct passwd getLFSPasswdEntry() {
+	char *buf;
+	size_t bufsize;
+	int pwdReturnCode;
+	struct passwd *lfsPasswdEntry;
+	struct passwd pwd;
+
+	bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
+	if (bufsize == -1)
+		bufsize = 16384;
+	buf = malloc(bufsize);
+	if (buf == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	pwdReturnCode = getpwnam_r("lfs", &pwd, buf, bufsize, &lfsPasswdEntry);
+
+	if (lfsPasswdEntry == NULL) {
+		if (pwdReturnCode == 0)
+			printf("Not found\n");
+		else {
+			errno = pwdReturnCode;
+			perror("getpwnam_r");
+		}
+		exit(EXIT_FAILURE);
+	}
+	return pwd;
+}
 
 int main() {
-	setuid(0);
-	setgid(0);
-	system("/bin/bash");
-	return 0;
+	struct passwd lfsPasswdEntry;
+	lfsPasswdEntry = getLFSPasswdEntry();
+	printf("%d\n", lfsPasswdEntry.pw_uid);
 }
