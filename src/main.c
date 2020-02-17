e1df790a569ce6f6b525424f889d0d25b5e4ab45
H8CKR <h8ckr@protonmail.com>
2020-02-17 23:12:21 +0100

    create user and group if not exist

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <grp.h>
 #include <errno.h>
 
 #define LFS "/mnt/lfs"
@@ -17,12 +19,49 @@ void requireRoot() {
 	}
 }
 
+void createLFSGroupIfNotExist() {
+	char *buf;
+	size_t bufsize;
+	const char *name;
+	struct group grp;
+	struct group *lfsGroupEntry;
+	int grpReturnCode;
+
+	bufsize = sysconf(_SC_GETGR_R_SIZE_MAX);
+	if (bufsize == -1)
+		bufsize = 16384;
+	buf = malloc(bufsize);
+	if (buf == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	name = "lfs";
+
+	grpReturnCode = getgrnam_r(name, &grp, buf, bufsize, &lfsGroupEntry);
+
+	if (lfsGroupEntry == NULL) {
+		if (grpReturnCode == 0) {
+			char *argv_list[] = {"lfs"};
+			execv("groupadd", argv_list);
+		}
+		else {
+			errno = grpReturnCode;
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		printf("using existing lfs group\n");
+	}
+}
+
 struct passwd getLFSPasswdEntry() {
 	char *buf;
 	size_t bufsize;
 	int pwdReturnCode;
 	struct passwd *lfsPasswdEntry;
 	struct passwd pwd;
+
+	createLFSGroupIfNotExist();
 
 	bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
 	if (bufsize == -1)
@@ -33,17 +72,24 @@ struct passwd getLFSPasswdEntry() {
 		exit(EXIT_FAILURE);
 	}
 
+	loadLFSUser:
+
 	pwdReturnCode = getpwnam_r("lfs", &pwd, buf, bufsize, &lfsPasswdEntry);
 
 	if (lfsPasswdEntry == NULL) {
 		if (pwdReturnCode == 0) {
 			perror("Could not find the lfs user in /etc/passwd");
+			char *argv_list[] = {"-s", "/bin/bash", "-g", "lfs", "-m", "-k","/dev/null", "lfs"};
+			execv("useradd", argv_list);
+			goto loadLFSUser;
 		}
 		else {
 			errno = pwdReturnCode;
 			perror("getpwnam_r");
 		}
 		exit(EXIT_FAILURE);
+	} else {
+		printf("using existing lfs user\n");
 	}
 	return pwd;
 }
