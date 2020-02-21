4408ed50534873363c7c7dddfbe4672a79b2151b
H8CKR <h8ckr@protonmail.com>
2020-02-21 01:13:13 +0100

    delete unused code

--- src/main.c
+++ src/main.c
@@ -22,57 +22,6 @@ void requireRoot() {
 		exit(EXIT_FAILURE);
 	}
 }
-/*
-void createLFSDirIfNotExist() {
-	int lfsDirReturnCode;
-
-	lfsDirReturnCode = mkdir(LFS, 0700);
-	if (lfsDirReturnCode == 0) {
-		printf("%s: directory created\n", LFS);
-	} else if(errno == EEXIST) {
-		printf("%s: directory exists\n", LFS);
-	} else {
-		char msg[100];
-		strcat(msg, "mkdir ");
-		strcat(msg, LFS);
-		perror(msg);
-		exit(EXIT_FAILURE);
-	}
-}
-
-void createToolsDirIfNotExist() {
-	int toolsDirReturnCode;
-
-	toolsDirReturnCode = mkdir(TOOLSDIR, 0700);
-	if (toolsDirReturnCode == 0) {
-		printf("%s: directory created\n", TOOLSDIR);
-	} else if (errno == EEXIST) {
-		printf("%s: directory exists\n", TOOLSDIR);
-	} else {
-		char msg[100];
-		strcat(msg, "mkdir ");
-		strcat(msg, TOOLSDIR);
-		perror(msg);
-		exit(EXIT_FAILURE);
-	}
-}
-
-void createSourcesDirIfNotExist() {
-	int sourcesDirReturnCode;
-
-	sourcesDirReturnCode = mkdir(SOURCESDIR, 0700);
-	if (sourcesDirReturnCode == 0) {
-		printf("%s: directory created\n", SOURCESDIR);
-	} else if (errno == EEXIST) {
-		printf("%s: directory exists\n", SOURCESDIR);
-	} else {
-		char msg[100];
-		strcat(msg, "mkdir ");
-		strcat(msg, SOURCESDIR);
-		perror(msg);
-		exit(EXIT_FAILURE);
-	}
-}*/
 
 void createLFSGroupIfNotExist() {
 	char *buf;
